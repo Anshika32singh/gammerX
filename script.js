50cc86b52dc09cf1613bb684a9bf5537387e5142
Anshika32singh <anshika32singh@gmail.com>
2025-02-05 22:25:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// You can add interactive features or animations here
+console.log("Welcome to GammerX - Enjoy Gaming!");
+
+// Smooth scroll effect for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
